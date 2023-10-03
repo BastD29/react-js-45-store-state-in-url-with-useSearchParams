d8b9cc76fb153f7b5f7b6ad8071cb3bebf4eda45
Bastien Daniel <bastien.daniel@rawfish.com>
2023-10-03 12:16:17 +0200

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Test1 from "./components/Test1";
 import Test2 from "./components/Test2";
 import Test3 from "./components/Test3";
+import Test4 from "./components/Test4";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Test3 />} />
+        <Route path="/" element={<Test4 />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Test4.jsx
+++ src/components/Test4.jsx
@@ -1,20 +1,24 @@
-import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { usePagination } from "../hooks/usePagination";
+import { useEffect, useState } from "react";
 import { useFilter } from "../hooks/useFilter";
 import { users } from "../data/users";
+import Pagination from "./Pagination";
 
-export default function Test3() {
+export default function Test4() {
   const navigate = useNavigate();
 
   const { city, age, name } = useFilter({ city: "all", age: "all", name: "" });
+  const { page, size } = usePagination(1, 2);
 
   const [filters, setFilters] = useState({ city, name, age });
+  const [currentPage, setCurrentPage] = useState(page);
 
   useEffect(() => {
     navigate({
-      search: `?city=${filters.city}&age=${filters.age}&name=${filters.name}`,
+      search: `?city=${filters.city}&age=${filters.age}&name=${filters.name}&page=${currentPage}&size=${size}`,
     });
-  }, [filters, navigate]);
+  }, [filters, currentPage, size, navigate]);
 
   // ** filtering logic to be moved into backend, only here for understanding
   const filteredUsers = users.filter((user) => {
@@ -27,9 +31,22 @@ export default function Test3() {
     );
   });
 
+  // ** pagination logic to be moved into backend, only here for understanding
+  const paginatedUsers = filteredUsers.slice(
+    (currentPage - 1) * size,
+    currentPage * size
+  );
+
   const handleFilterChange = (key, value) => {
     setFilters((prev) => ({ ...prev, [key]: value }));
+    setCurrentPage(1); // reset to first page when filters change
   };
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const totalPages = Math.ceil(filteredUsers.length / size);
 
   return (
     <>
@@ -66,12 +83,17 @@ export default function Test3() {
         />
       </label>
       <ul>
-        {filteredUsers.map((user) => (
+        {paginatedUsers.map((user) => (
           <li key={user.id}>
             {user.name} - {user.age} - {user.city}
           </li>
         ))}
       </ul>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
     </>
   );
 }
